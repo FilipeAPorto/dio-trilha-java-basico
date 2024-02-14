06ba88b919972ecfcfecdde4728d8d598dba0d14
Filipe Alves Porto <filipealvesmissias@hotmail.com>
2024-02-14 16:20:38 -0300

    Construtores

---
+++ Aulas/src/com/modulo3/construtores/Pessoa.java
@@ -0,0 +1,32 @@
+package com.modulo3.construtores;
+
+public class Pessoa {
+
+    private String nome,cpf,endereco;
+
+    /*
+    Para criar um Costrutor, quais são os atributos necessarios para a criação da classe em questão,
+    nesse exemplo temos o Nome e CPF como essênciais, logo eles serão solicitados no costrutor e não por Setters
+    Não usar todos os atributos no construtor, isso é uma má pratica e pode dar problemas depois.
+     */
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
---
+++ Aulas/src/com/modulo3/construtores/SistemaCadastro.java
@@ -0,0 +1,16 @@
+package com.modulo3.construtores;
+
+public class SistemaCadastro {
+    public static void main(String[] args) {
+
+        Pessoa pessoa = new Pessoa("Juão","695.984.621-74");
+
+        pessoa.setEndereco("Rua Avila Salgueiro Filho - 321");
+
+
+        System.out.println("Nome : " + pessoa.getNome());
+        System.out.println("CPF : " + pessoa.getCpf());
+        System.out.println("CEP " + pessoa.getEndereco());
+
+    }
+}
